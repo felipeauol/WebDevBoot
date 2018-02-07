8c02fa7589d939d87ed3eddd0c17854ac35e7f4c
Felipe de Oliveira <felipe.auol@gmail.com>
2018-02-06 21:14:30 -0500

    added random color logic

--- Projects/Color Game/color.js
+++ Projects/Color Game/color.js
@@ -1,12 +1,5 @@
 // Build array to contain background colors for each square
-var colors = [
-    "rgb(255, 0, 0)",
-    "rgb(255, 255, 0)",
-    "rgb(0, 255, 0)",
-    "rgb(0, 255, 255)",
-    "rgb(0, 0, 255)",
-    "rgb(255, 0, 255)"
-]
+var colors = generateColorArray(6);
 
 //Select color to display on the title
 var pickedColor = pickColor();
@@ -17,6 +10,7 @@ displayColor.textContent = pickedColor;
 // Select other elements
 var squares = document.querySelectorAll(".square");
 var messageDisplay = document.querySelector("#message");
+var header = document.querySelector("#header");
 
 for(var i = 0; i < squares.length; i++){
     squares[i].style.backgroundColor = colors[i];
@@ -26,9 +20,12 @@ for(var i = 0; i < squares.length; i++){
 for (let i = 0; i < squares.length; i++) {
     squares[i].addEventListener("click", 
     function(){
+        //Win case
         if (this.style.backgroundColor === pickedColor) {
             messageDisplay.textContent = "Correct!";
+            header.style.background = pickedColor;
             changeColors(pickedColor);
+        //Lose case
         } else{
             this.style.background = "#232323";
             messageDisplay.textContent = "Try Again"; 
@@ -46,10 +43,37 @@ function changeColors(color) {
     }
 }
 
-//Function to pick a random color
+//Function to pick a random color from array
 function pickColor() {
     //pick a random array index
     let random = Math.floor(Math.random() * colors.length)
     //select color from that index
     return colors[random];
+}
+
+//Function to generate an array of colors
+function generateColorArray(n) {
+//create an array
+    var arr = [];
+//generate n rgb colors
+    for (let i = 0; i < n; i++) {
+        //add random color to array
+        arr.push(randomColor());
+    }
+//return array
+    return arr;   
+}
+
+//Function to generate a random rgb color
+function randomColor() {
+    //generate a value for r
+    let r = Math.floor(Math.random() * 256);
+    //generate a value for g
+    let g = Math.floor(Math.random() * 256);
+    //generate a value for b
+    let b = Math.floor(Math.random() * 256);
+    //form string
+    color = "rgb(" + r + ", " + g + ", " + b + ")";
+    //return color as string
+    return color;
 }
