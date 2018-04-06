4047f511bfe112f6c5a760f936ea31de625c940c
Felipe_Oliveira <felipe.auol@gmail.com>
2018-04-05 21:47:19 -0400

    EJS Conditionals and Loops

--- IntrotoNode/MoreExpress/EJSDemo/app.js
+++ IntrotoNode/MoreExpress/EJSDemo/app.js
@@ -6,13 +6,21 @@ app.get("/",function(req, res) {
     res.send("Welcome to our home page");
 })
 
-// Dynamic page for city you want to visit
+// Dynamic page for a city you want to visit
 app.get("/visit/:city",function(req, res){
     city = req.params.city;
     myCity = city.toUpperCase();
-    
-    
+
     res.render('tours.ejs', {city: myCity});
+})
+
+// List the city you want to visit n times
+app.get("/visit/:city/:number", function(req, res){
+    city = city = req.params.city;
+    myCity = city.toUpperCase();
+    n = req.params.number;
+
+    res.render('tours.ejs', {city: myCity, repeat: n});
 })
 
 // Start Server
